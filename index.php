4275ca1e9d1b6cca19093711ec02ea000b351fd9
railopes <rai.zeckk@gmail.com>
2019-09-17 22:27:55 -0300

    tela de inicio do sistema - login

--- index.php
+++ index.php
@@ -1,46 +1,61 @@
+<?php 
+session_start();
+?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>UMC-Brinquedoteca</title>
-	<script src="utils.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UmcBrinquedoteca</title>
+    <link rel="stylesheet" href="CSS/inicio.css">
+    <script src="utils.js"></script>
 </head>
 <body>
-	<table>
-		<tr class="bolota">
-			<td>NOME</td>
-			<td>SENHA</td>
-			<td>ID</td>
-		</tr>
-		<tr class="bolota">
-			<td>Antonio Rai F. Lopes</td>
-			<td>railopes786</td>
-			<td>010</td>
-		</tr>
-		<tr class="bolota">
-			<td>renan F. Lopes</td>
-			<td>renandoidao</td>
-			<td>011</td>
-		</tr>
-	</table>
-<script>
-	function sendTable(){
-		const minha_tabela = document.getElementsByTagName('table')[0];
-		const jsonTable = tableToJson(minha_tabela);
-		let http = {
-			method:'POST',
-			url:"http://localhost/exportexcel.php",
-			body:jsonTable,
-			header:null,
-		}
-		$ajax.request(http,(res)=>{
-			console.log(res.response);
-		})
-
-	}
-</script>
-
-<input type="button" value="SendMyHTML" onclick="sendTable();">
-
+    <form action="/login.php" method="post" id='form_login'>
+        <div id='Logo' width="200"></div>
+        <input type="text" name="user_name" id="form_user">
+        
+        <input type="password" name="user_password" id="form_pass">
+        
+        <input type="submit" value="Logar-Se">
+        <a href="#">Esqueci Minha Senha!</a>
+    </form>
+    <input type="button" value="SHOW TABELA" id='showmytable'>
+    <div id="GV"></div>
 </body>
+
+<script>
+    (function(){
+        const FORM_LOGIN = document.querySelector("#form_login");   
+            FORM_LOGIN.addEventListener('onsubmit',(ev)=>{
+                const form_user = document.querySelector("#form_user");
+                const form_pass = document.querySelector("#form_pass");
+
+                if(form_user.value === null || form_user.value === undefined || form_user.value ='' ){
+                    form_user
+                }
+            });
+    })()
+    (function(){
+        
+        document.querySelector('#showmytable').addEventListener('click',(e)=>{
+            const http ={
+                url:'login.php',
+                method:'GET',
+                body:null,
+                loading:function(){
+                    
+                }
+            }
+            $ajax.request(http,(response,XHR)=>{
+                const mydiv = document.querySelector('#GV');
+                makeGridView(JSON.parse(XHR.res),mydiv)
+            });
+        });
+        //console.clear();
+    })();
+
+    // fetch('login.php').then(function(response) { response.text().then(function(res){console.log(JSON.parse(res))})})
+</script>
 </html>
