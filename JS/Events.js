1e79ff4f345c3ed2bcfe5d2d65ba25159527fa37
railopes <rai.zeckk@gmail.com>
2019-09-17 23:39:41 -0300

    Adcionado funcao de timer

--- JS/Events.js
+++ JS/Events.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded',()=>{
-
-});
+var x =10;
+var timerdiv = document.querySelector('#timer_div');
+function timer(){
+  timerdiv.innerHTML = x;
+  if((x-1)>0){
+    setTimeout(function(){timer();},1000);
+  }else{
+    window.location.href = 'http://localhost';
+  }
+  
+  x--;
+}
+timer();
