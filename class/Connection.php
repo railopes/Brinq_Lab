227fb24e2544eb3ab33ec9a09c849916ae1f35b5
railopes <rai.zeckk@gmail.com>
2019-09-19 00:59:01 -0300

    redefinido confirguraçõe de conexao ao BD

--- class/Connection.php
+++ class/Connection.php
@@ -1,8 +1,8 @@
 <?php 
-define('HOST','127.0.0.1');
-define('USER','root');
-define('USER_PASS','');
-define('MYDB','UMC_BRINQUEDOTECA');
+define('HOST','development-umc.mysql.uhserver.com');
+define('USER','coordenacao');
+define('USER_PASS','Umc@508');
+define('MYDB','development_umc');
 function executeDb($sql,$typeQuery,$mode = 5,$values = Array()){
 	// echo HOST."<br/>".USER."<br/>".USER_PASS."<br/>".MYDB."<br/>";
 	$conn = mysqli_connect(HOST,USER,USER_PASS,MYDB);
