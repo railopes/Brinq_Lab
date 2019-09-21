3e1cf6ea60e133a96f0cc05eca77ec2ec5b002e0
railopes <rai.zeckk@gmail.com>
2019-09-21 00:49:46 -0300

    pasta com os controles do sistema - chamadas

---
+++ res/login.php
@@ -0,0 +1,42 @@
+
+<?php 
+session_start();
+if(!empty($_SESSION) && isset($_SESSION)){
+    if($_SESSION['name'] == false){
+            header('Location: ./index.php');
+            exit();        
+    }
+}
+require_once("class/Connection.php");
+if(isset($_POST) && !empty($_POST)){
+    $user = preg_replace('/[^[:alnum:]_\s]/', '',$_POST['user_name']);
+    $password = md5($_POST['user_password']);
+    $newquery = "SELECT * FROM usuarios WHERE nome = '$user' AND senha ='$password'";
+    // header("Content-type: Application/JSON");
+    $resposta =  ["dataTable"=>executeDb($newquery,true,4)];
+    if($resposta['dataTable']['afected_rows'] == 1){
+        $_SESSION['name'] = $resposta['dataTable']['row']['nome'];
+        $_SESSION['profileVersion']= $resposta['dataTable']['row']['perfil'];
+    }else{
+        $bult =true;
+    }
+
+}
+if(!isset($bult)&& !empty($bult)){
+    if(!$bult){
+        echo json_encode($resposta);
+    }
+    
+    
+}else{
+    if(isset($_SESSION) && !empty($_SESSION)){
+        echo "<br/><a href='logout.php'>Desloga-se</a>"; 
+    }else{
+        $_SESSION['name'] = false;
+        header("Location: ./index.php");
+        exit();
+    }
+}
+unset($_POST);
+
+?>
