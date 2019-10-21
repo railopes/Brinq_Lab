cd6243d91a067ca0708e509f102fb57783f6ce22
railopes <rai.zeckk@gmail.com>
2019-10-20 21:52:43 -0300

    refatorado par a v2 do API

--- API/cadastro.php
+++ API/cadastro.php
@@ -1,17 +1,47 @@
 <?php
+session_start();
 header("Access-Control-Allow-Origin: *");
-function varIsValid($var_){
-  $retorno =  (isset($var_) && !empty($var_));
-  return $retorno;
+$DIR = "./../class";
+$estaLogado = require $DIR."/logged.php";
+require_once($DIR."/Usuarios.class.php");
+
+try{
+  $_POST = @json_decode(file_get_contents("php://input"))->body;
+}catch(Exception $e){}
+
+if(!$estaLogado){
+  if(isset($_POST)){
+    http_response_code(403);
+    header('Content-type: application/json');
+    echo json_encode(["Error"=>"user can't have permissions"]);
+    exit();
+  }else{
+    header("Location: /");
+    exit();
+  }
 }
+
 header('Content-type: application/json');
-
-$_POST = file_get_contents("php://input");
-if(varIsValid($_POST)){
-  echo json_encode(['nome'=>'raiLopes','data'=>json_decode($_POST)]);
-
+if(
+    ( isset($_GET['cadTipo']) && !empty($_GET['cadTipo']) ) &&
+    ( isset($_POST) && !empty($_POST))
+  ){
+  switch($_GET['cadTipo']){
+    case 1:
+      $U = new Usuarios();
+      $Uname = preg_replace('/[^[:alnum:]_\s]/', '',$_POST->name);
+      echo json_encode( $U::cadastrar($Uname,md5($_POST->pass_),$_POST->access) );
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+  }
 }else{
-  echo  json_encode(["Error"=>'body request not send']);
+  http_response_code(400);
+  echo json_encode(["Error"=>"Body not sent or 'cadTipo' has not specified."]);
+
 }
-http_response_code(200)
+
+
 ?>
