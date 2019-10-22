69fe0b20b542d11e7831c84d580dcd32b7ee717e
railopes <rai.zeckk@gmail.com>
2019-10-22 00:39:46 -0300

    troca barras do css

--- home.php
+++ home.php
@@ -15,8 +15,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LPP - UMC Universidade</title>
-    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="./bootstrap/fontawesome/css/font-awesome.css" rel="stylesheet">
+    <link href=".\bootstrap\css\bootstrap.min.css" rel="stylesheet">
+    <link href=".\bootstrap\fontawesome\css\font-awesome.css" rel="stylesheet">
 
 
     <!-- dattables dependeces -->
